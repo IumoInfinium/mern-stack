288f889c6d9f42a8299605b716d0cd954e341a15
Harshit <harshitpanari@gmail.com>
2023-04-16 21:32:20 +0530

    testing

--- client/src/App.js
+++ client/src/App.js
@@ -15,8 +15,8 @@ function App() {
   }, []);
 
   async function fetchTransctions() {
-    const res = await fetch("http://localhost:4000/transaction");
-    //const res = await fetch("https://mern-stack-kyet.vercel.app/");
+    //const res = await fetch("http://localhost:4000/transaction");
+    const res = await fetch("https://mern-stack-kyet.vercel.app/transaction");
     const { data } = await res.json();
     settransactions(data);
   }
